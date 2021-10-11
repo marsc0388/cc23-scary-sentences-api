d3e59b719a67f1c48efc16836d6d0cd64b0cd7b2
Cal <marsc0388@gmail.com>
2021-10-12 08:32:27 +0900

    Feat: Add cors to allow front end to make queries

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
 const context = require('./context')
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     context: context,
     graphiql: true
 }))
+
 
 const PORT = 5000 || process.env.PORT;
 app.listen(5000., () => {
